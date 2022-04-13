7f82700c67d58387de4da0e3651bdc3fdfde9894
Circle Chen <circlecly@berkeley.edu>
2022-04-12 20:04:23 -0700

    Evasion AI

---
+++ Assets/Scripts/Entities/CompetitorSensor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompetitorSensor : MonoBehaviour
+{
+    public bool acidDetected = false;
+    public Vector3 acidPosition;
+
+    public bool baseDetected = false;
+    public Vector3 basePosition;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Acid"))
+        {
+            Debug.Log(acidPosition);
+            acidDetected = true;
+            acidPosition = collision.gameObject.transform.position;
+        }
+        else if (collision.gameObject.CompareTag("Base"))
+        {
+            baseDetected = true;
+            basePosition = collision.gameObject.transform.position;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Acid"))
+        {
+            acidDetected = false;
+        }
+        else if (collision.gameObject.CompareTag("Base"))
+        {
+            baseDetected = false;
+        }
+    }
+}
--- Assets/Scripts/Entities/CompetitorSpermAI.cs
+++ Assets/Scripts/Entities/CompetitorSpermAI.cs
@@ -9,23 +9,52 @@ public class CompetitorSpermAI: MonoBehaviour
 
     private GameObject _target;
 
+    #region General movement
+
     public float moveSpeed;
 
     public float rotationSpeed;
 
     private float _reachThres = 0.1f;
 
+
+
+    #endregion
+
+    #region Obstacle evasion
+    private CompetitorSensor _sensor;
+
+    private PHControl _pHControl;
+
+    public float evasionImportance;
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
+        _pHControl = GetComponent<PHControl>();
+        _sensor = GetComponentInChildren<CompetitorSensor>();
         _target = firstWaypoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveDir = _target.transform.position - transform.position;
-        float angle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
+        Vector2 moveDir = (_target.transform.position - transform.position).normalized;
+        float waypointAngle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
+        Vector2 evadeDir = GetEvadeDirection();
+        float angle;
+        if (evadeDir.Equals(Vector2.zero))
+        {
+            angle = waypointAngle;
+        } else
+        {
+            float evasionAngle = Mathf.Atan2(evadeDir.y, evadeDir.x) * Mathf.Rad2Deg;
+            angle = waypointAngle * (1 - evasionImportance) + evasionAngle * evasionImportance;
+        }
+        
+        
         Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
         transform.position = transform.position + Quaternion.Euler(0, 0, angle) * new Vector2(1, 0) * moveSpeed * Time.deltaTime;
@@ -36,4 +65,18 @@ public class CompetitorSpermAI: MonoBehaviour
 
     }
 
+    Vector2 GetEvadeDirection()
+    {
+        float pHPercent = (_pHControl.PH - _pHControl.minPH) / (_pHControl.maxPH - _pHControl.minPH);
+        if (pHPercent < 0.5 && _sensor.acidDetected)
+        {
+            return Quaternion.Euler(0, 0, 180) * (_sensor.acidPosition - transform.position);
+        } else if (pHPercent >= 0.5 && _sensor.baseDetected)
+        {
+            return Quaternion.Euler(0, 0, 180) * (_sensor.basePosition - transform.position);
+        }
+        return new Vector3(0, 0, 0);
+    }
+
+    
 }
